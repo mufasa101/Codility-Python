eec6c19337d0276cb81111bbb44b302d8ac9577c
Joseph Disraeli Omenda <joseph.omenda007@gmail.com>
2021-11-26 09:57:55 +0300

    Update MissingInteger.py

--- MissingInteger.py
+++ MissingInteger.py
@@ -1,6 +1,16 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
-
+def solution(A):
+    A.sort()
+    # print("soreted",A)
+    if len(A)< 1:
+        return 1
+    for i in range(len(A)):
+        if A[i]!=i+1:
+            return i+1
+        
+    return A[len(A)-1]+1
+    return (len(A) + 1 )
 def solution(A):
     # write your code in Python 3.6
     
