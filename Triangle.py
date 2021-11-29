736c8c600b65b31e36d576fb3a99a1194b832c34
Joseph Disraeli Omenda <joseph.omenda007@gmail.com>
2021-11-29 17:37:19 +0300

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -1,3 +1,14 @@
+
+def solution(A):
+    # write your code in Python 3.6
+    if len(A)<3:
+        return 0
+    A.sort()
+    for i in range(0,len(A)-2):
+        if (A[i]+A[i+1]) > A[i+2]:
+            return 1
+    return 0
+
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
