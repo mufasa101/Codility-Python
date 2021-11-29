f2f3f2129752a650109dca7cc50f5390e269f35b
Joseph Disraeli Omenda <joseph.omenda007@gmail.com>
2021-11-29 17:12:51 +0300

    Update MaxProductOfThree.py

--- MaxProductOfThree.py
+++ MaxProductOfThree.py
@@ -1,3 +1,11 @@
+def solution(A):
+    A.sort()
+    # N=len(A)
+    data1=A[-1]*A[-2]*A[-3]
+    data2=A[-1]*A[0]*A[1]
+    final=max(data1,data2)
+    return final
+
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
